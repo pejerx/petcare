e72f41248d406a3dc75e95a4a46399c9ec63267a
pejerx <jerspooh@gmail.com>
2025-06-26 13:12:33 +0800

    Updated nani

--- backend/petcare-backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/petcare-backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -1,14 +1,14 @@
 package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
